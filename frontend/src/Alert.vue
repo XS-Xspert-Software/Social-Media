3d568bcf811a76b3f571f023a631d1410eed2fac
endless296 <name86473@gmail.com>
2025-07-22 17:10:00 +0530

    Create Alert.vue

---
+++ frontend/src/Alert.vue
@@ -0,0 +1,24 @@
+<template>
+  <div :class="['Alert', { 'error-Alert': isError, 'show-Alert': show }]">
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const message = ref('')
+const show = ref(false)
+const isError = ref(false)
+
+function showAlert(msg, error = false) {
+  message.value = msg
+  isError.value = error
+  show.value = true
+  setTimeout(() => {
+    show.value = false
+  }, 2500)
+}
+
+defineExpose({ showAlert }) 
+</script>
