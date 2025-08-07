6904e86854d2115d803194737bb92da97931e302
endless296 <name86473@gmail.com>
2025-08-07 16:19:49 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,28 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 export default defineConfig({
+  base: './',
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('vue')) return 'vue-vendor';
+            if (id.includes('pinia')) return 'pinia-vendor';
+            if (id.includes('lodash')) return 'lodash-vendor';
+            if (id.includes('ably')) return 'ably-vendor';
+            return 'vendor'; 
+          }
+        },
+      },
+    },
+  },
 });
