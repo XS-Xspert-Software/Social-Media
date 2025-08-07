80201ba0ed268356395619fcf682ef8f0510b2a0
endless296 <name86473@gmail.com>
2025-08-07 16:15:02 +0530

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,18 +4,9 @@ import './style.css';
 import router from './router/index.js';
 import { createPinia } from 'pinia';
 
-import PrimeVue from 'primevue/config';
-import Aura from '@primeuix/themes/aura';
-
 const app = createApp(App);
 
 const pinia = createPinia();
-
-app.use(PrimeVue, {
-  theme: {
-    preset: Aura
-  }
-});
 
 app.use(router);
 app.use(pinia);
